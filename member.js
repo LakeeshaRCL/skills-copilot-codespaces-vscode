48739e7ec5d7b645ed4af12ff37a46a71b3cf7a9
Lakeesha Ramanayake <65113676+LakeeshaRCL@users.noreply.github.com>
2023-08-02 16:27:55 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember(){
+    return {
+        restrict: 'E',
+        templateUrl: 'modules/skills/views/member.html',
+        controller: 'SkillsmemberController',
+        controllerAs: 'vm',
+        bindToController: true,
+        scope: {
+            member:'='
+        }
+    };
+}
